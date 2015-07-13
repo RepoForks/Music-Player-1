c13bf5ee1bca63c6f29afc6209595ebd2520651e
Andrew <jydimir@gmail.com>
2015-07-13 18:03:36 +0300

    retaining fragments added to NavigationView

--- app/src/main/java/ua/edu/cdu/fotius/lisun/musicplayer/NavigationActivity.java
+++ app/src/main/java/ua/edu/cdu/fotius/lisun/musicplayer/NavigationActivity.java
@@ -1,24 +1,17 @@
 package ua.edu.cdu.fotius.lisun.musicplayer;
 
-import android.app.Activity;
 import android.media.AudioManager;
-import android.os.PersistableBundle;
+import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
-import android.widget.FrameLayout;
-import android.widget.SeekBar;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import ua.edu.cdu.fotius.lisun.musicplayer.ua.edu.cdu.fotius.lisun.musicplayer.fragments.AlbumsBrowserFragment;
@@ -39,7 +32,7 @@ public class NavigationActivity extends AppCompatActivity {
     private final String CURRENT_FRAGMENT_TAG_KEY = "current_fragment_tag";
     private Fragment mCurrentFragment;
 
-    private String mCurrentFragmentTag;
+    //private String mCurrentFragmentTag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,32 +47,32 @@ public class NavigationActivity extends AppCompatActivity {
         //if activity recreating previous state get fragment
         //which was saved on destroing previous state
         if(savedInstanceState != null) {
-            mCurrentFragmentTag = savedInstanceState.getString(CURRENT_FRAGMENT_TAG_KEY);
-            mCurrentFragment = getSupportFragmentManager().findFragmentByTag(mCurrentFragmentTag);
+            mCurrentFragment = getSupportFragmentManager()
+                    .findFragmentByTag(savedInstanceState.getString(CURRENT_FRAGMENT_TAG_KEY));
         }
 
         //if activity runs for the first time set Songs fragment as
         //initial fragment
         if(mCurrentFragment == null) {
             mCurrentFragment = new SongsBrowserFragment();
-            mCurrentFragmentTag = SONGS_BROWSER_FRAGMENT_TAG;
             getSupportFragmentManager().beginTransaction()
-                .add(R.id.fragment_container, mCurrentFragment, mCurrentFragmentTag)
+                .add(R.id.fragment_container, mCurrentFragment, SONGS_BROWSER_FRAGMENT_TAG)
                 .commit();
         }
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
-        outState.putString(CURRENT_FRAGMENT_TAG_KEY, mCurrentFragmentTag);
+        outState.putString(CURRENT_FRAGMENT_TAG_KEY, mCurrentFragment.getTag());
         super.onSaveInstanceState(outState);
     }
 
     private void setUpToolbarAndNavigationView() {
+
         final NavigationView navigationView =
                 (NavigationView) findViewById(R.id.navigation_view);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
         if(toolbar != null) {
             setSupportActionBar(toolbar);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -88,7 +81,6 @@ public class NavigationActivity extends AppCompatActivity {
             toolbar.setNavigationOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
                     drawer.openDrawer(navigationView);
                 }
             });
@@ -96,78 +88,66 @@ public class NavigationActivity extends AppCompatActivity {
 
         //Even if toolbar will be null, we can use navigation view without it
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
 
+                //if tending to replace with the same fragment don't do this, because this will call
+                //Fragment#OnCreate() where database time consuming operations are performed
                 switch (menuItem.getItemId()){
                     case R.id.navigation_item_artists:
-
-                        //check if fragment already exsists
-                        mCurrentFragment
-                                = (ArtistsBrowserFragment)getSupportFragmentManager()
-                                .findFragmentByTag(ARTISTS_BROWSER_FRAGMENT_TAG);
-
-                        if(mCurrentFragment == null) {
-                            mCurrentFragment = new ArtistsBrowserFragment();
+                        if(!isSameFragment(ARTISTS_BROWSER_FRAGMENT_TAG)) {
+                            replaceFragment(new ArtistsBrowserFragment(),
+                                    ARTISTS_BROWSER_FRAGMENT_TAG);
                         }
-
-                        replaceFragment(mCurrentFragment, ARTISTS_BROWSER_FRAGMENT_TAG);
-
                         break;
                     case R.id.navigation_item_albums:
-
-                        mCurrentFragment
-                                = (AlbumsBrowserFragment)getSupportFragmentManager()
-                                .findFragmentByTag(ALBUMS_BROWSER_FRAGMENT_TAG);
-
-                        if(mCurrentFragment == null) {
-                            mCurrentFragment = new AlbumsBrowserFragment();
+                        if(!isSameFragment(ALBUMS_BROWSER_FRAGMENT_TAG)) {
+                            replaceFragment(new AlbumsBrowserFragment(),
+                                    ALBUMS_BROWSER_FRAGMENT_TAG);
                         }
-
-                        replaceFragment(mCurrentFragment, ALBUMS_BROWSER_FRAGMENT_TAG);
-
                         break;
                     case R.id.navigation_item_songs:
-                        mCurrentFragment
-                                = (AlbumsBrowserFragment)getSupportFragmentManager()
-                                .findFragmentByTag(SONGS_BROWSER_FRAGMENT_TAG);
-
-                        if(mCurrentFragment == null) {
-                            mCurrentFragment = new SongsBrowserFragment();
+                        if(!isSameFragment(SONGS_BROWSER_FRAGMENT_TAG)) {
+                            replaceFragment(new SongsBrowserFragment(),
+                                    SONGS_BROWSER_FRAGMENT_TAG);
                         }
-
-                        replaceFragment(mCurrentFragment, SONGS_BROWSER_FRAGMENT_TAG);
                         break;
                     case R.id.navigation_item_my_playlists:
-                        mCurrentFragment
-                                = (AlbumsBrowserFragment)getSupportFragmentManager()
-                                .findFragmentByTag(MY_PLAYLISTS_BROWSER_FRAGMENT_TAG);
-
-                        if(mCurrentFragment == null) {
-                            mCurrentFragment = new MyPlaylistsBrowserFragment();
+                        if(!isSameFragment(MY_PLAYLISTS_BROWSER_FRAGMENT_TAG)) {
+                            replaceFragment(new MyPlaylistsBrowserFragment(),
+                                    MY_PLAYLISTS_BROWSER_FRAGMENT_TAG);
                         }
-
-                        replaceFragment(mCurrentFragment, MY_PLAYLISTS_BROWSER_FRAGMENT_TAG);
                         break;
                     case R.id.navigation_item_settings:
-                        //TODO: call settings fragment
+                        //TODO: initialize selectedFragment
                         Toast.makeText(NavigationActivity.this, "Settings", Toast.LENGTH_SHORT).show();
                         break;
                 }
 
-                //TODO:Debug
-                Log.d(TAG, "Fragment tag:" + mCurrentFragment.getTag() + "\tFragment ID: " + mCurrentFragment.getId());
+                drawer.closeDrawers();
 
+                //TODO: when rotating selected checking is disappeared
+                menuItem.setChecked(true);
+                //true to display item as selected
+                return true;
+            }
+
+            private boolean isSameFragment(String fragmentTag) {
+                if((mCurrentFragment.getTag()).equals(fragmentTag)) {
+                    return true;
+                }
                 return false;
             }
         });
     }
-
     private void replaceFragment(Fragment fragment, String fragmentTag) {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, fragment, fragmentTag)
                 .commit();
-        mCurrentFragmentTag = fragmentTag;
+        mCurrentFragment = fragment;
+
+        Log.d(TAG, "replaced fragment TAG: " + mCurrentFragment.getTag());
     }
 
     @Override
