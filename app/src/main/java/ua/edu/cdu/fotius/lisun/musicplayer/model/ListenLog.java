552f1d7b4941243711baa5f4aee009b12de7cdbc
Andrew <jydimir@gmail.com>
2016-03-05 12:03:49 +0200

    Added genre, artist, album attributes to model

--- app/src/main/java/ua/edu/cdu/fotius/lisun/musicplayer/model/ListenLog.java
+++ app/src/main/java/ua/edu/cdu/fotius/lisun/musicplayer/model/ListenLog.java
@@ -7,11 +7,17 @@ import io.realm.annotations.PrimaryKey;
 public class ListenLog extends RealmObject{
 
     public static String TRACK_ID_ATTR = "trackId";
+    public static String GENRE_ATTR = "genre";
+    public static String ARTIST_ATTR = "artist";
+    public static String ALBUM_ATTR = "listenedCounter";
     public static String LISTENED_COUNTER_ATTR = "listenedCounter";
+
 
     @PrimaryKey
     private long trackId;
-
+    private String genre;
+    private String artist;
+    private String album;
     private long listenedCounter;
 
     public long getTrackId() {
@@ -28,5 +34,29 @@ public class ListenLog extends RealmObject{
 
     public void setListenedCounter(long listenedCounter) {
         this.listenedCounter = listenedCounter;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
     }
 }
--- app/src/main/java/ua/edu/cdu/fotius/lisun/musicplayer/model/ListenedUpdaterAsyncTask.java
+++ app/src/main/java/ua/edu/cdu/fotius/lisun/musicplayer/model/ListenedUpdaterAsyncTask.java
@@ -1,11 +1,16 @@
 package ua.edu.cdu.fotius.lisun.musicplayer.model;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.provider.MediaStore;
 import android.util.Log;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
+import ua.edu.cdu.fotius.lisun.musicplayer.utils.AudioStorage;
 
 public class ListenedUpdaterAsyncTask extends AsyncTask<Long, Void, Void> {
 
@@ -25,22 +30,90 @@ public class ListenedUpdaterAsyncTask extends AsyncTask<Long, Void, Void> {
         ListenLog justListenedLog  =
                 mRealm.where(ListenLog.class).equalTo(ListenLog.TRACK_ID_ATTR, trackId).findFirst();
 
-        mRealm.beginTransaction();
         if(justListenedLog == null) {
+            ContentResolver resolver = mContext.getContentResolver();
+            String genre = retrieveGenre(resolver, trackId);
+            String artist = retrieveArtist(resolver, trackId);
+            String album = retrieveAlbum(resolver, trackId);
+
+            mRealm.beginTransaction();
             ListenLog newLog = mRealm.createObject(ListenLog.class);
             newLog.setTrackId(trackId);
             newLog.setListenedCounter(1);
+            newLog.setGenre(genre);
+            newLog.setArtist(artist);
+            newLog.setAlbum(album);
+            mRealm.commitTransaction();
         } else {
+            mRealm.beginTransaction();
             long counter = justListenedLog.getListenedCounter();
             justListenedLog.setListenedCounter(++counter);
+            mRealm.commitTransaction();
         }
-        mRealm.commitTransaction();
+
 
         //TODO: debug only
         RealmResults<ListenLog> results = mRealm.allObjects(ListenLog.class);
         for(ListenLog l : results) {
-            Log.d(TAG, "ID : " + l.getTrackId() + " COUNTER: " + l.getListenedCounter());
+            Log.d(TAG, "ID : " + l.getTrackId() + " COUNTER: " + l.getListenedCounter() +
+                    " GENRE: " + l.getGenre() + " ARTIST: " + l.getArtist() + " ALBUM: " + l.getAlbum());
         }
         return null;
+    }
+
+    private String retrieveGenre(ContentResolver contentResolver, long trackId) {
+        Uri uri = MediaStore.Audio.Genres.getContentUriForAudioId("external", (int) trackId);
+        String[] projection = new String[] {
+                AudioStorage.Genres.GENRE
+        };
+        Cursor cursor = contentResolver.query(uri, projection, null, null, null);
+        String genre = null;
+        if(cursor != null) {
+            if(cursor.moveToFirst()) {
+                genre = cursor.getString(0);
+            }
+            cursor.close();
+        }
+        return genre;
+    }
+
+    private String retrieveArtist(ContentResolver contentResolver, long trackId) {
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = new String[] {
+                AudioStorage.Track.ARTIST
+        };
+        String selection = AudioStorage.Track.TRACK_ID + "=?";
+        String[] selectionArgs = new String[] {
+                Long.toString(trackId)
+        };
+        Cursor cursor = contentResolver.query(uri, projection, selection, selectionArgs, null);
+        String artist = null;
+        if(cursor != null) {
+            if (cursor.moveToFirst()) {
+                artist = cursor.getString(0);
+            }
+            cursor.close();
+        }
+        return artist;
+    }
+
+    private String retrieveAlbum(ContentResolver contentResolver, long trackId) {
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = new String[] {
+                AudioStorage.Track.ALBUM
+        };
+        String selection = AudioStorage.Track.TRACK_ID + "=?";
+        String[] selectionArgs = new String[] {
+                Long.toString(trackId)
+        };
+        Cursor cursor = contentResolver.query(uri, projection, selection, selectionArgs, null);
+        String album = null;
+        if(cursor != null) {
+            if (cursor.moveToFirst()) {
+                album = cursor.getString(0);
+            }
+            cursor.close();
+        }
+        return album;
     }
 }
